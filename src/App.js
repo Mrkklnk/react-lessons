05370de6b6a39655496f31f4baf97aecb012e95b
Mrkklnk <mrkk1nk1@gmail.com>
2023-10-24 13:19:25 +0200

    lesson3

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import {PostContainer} from "./components/PostsContainer/PostContainer";
 
 const App = () => {
   return (
       <div>
-          App
+          <PostContainer/>
       </div>
   );
 };

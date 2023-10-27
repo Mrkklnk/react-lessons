8e43f572e2ed72b7e4b6a471ba2278fa5c553901
Mrkklnk <mrkk1nk1@gmail.com>
2023-10-27 15:07:56 +0200

    lesson3_hw1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import {User} from "./components/UsersContainer/User";
+import {UsersContainer} from "./components/UsersContainer/UsersContainer";
+import {CommentsContainer} from "./components/CommentsContainer/CommentsContainer";
 
 const App = () => {
   return (
-      <div>
-          App
+      <div style={{display: "flex"}}>
+          <UsersContainer/>
+          <CommentsContainer/>
       </div>
   );
 };

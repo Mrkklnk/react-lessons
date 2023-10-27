9114903a2aeb627b5064957442bb83517fe603fc
Mrkklnk <mrkk1nk1@gmail.com>
2023-10-27 14:07:46 +0200

    lesson3partT2

--- src/components/CarsContainer/CarForm.js
+++ src/components/CarsContainer/CarForm.js
@@ -22,31 +22,13 @@ const CarForm = ({setTrigger, carForUpdate}) => {
 
     const save = async (car) => {
         console.log(car);
-        await carService.crate(car);
+        await carService.create(car);
         setTrigger(prev => !prev)
         reset()
     }
 
     return (
         <>
-            {/*<form onSubmit={handleSubmit(save)}>*/}
-            {/*    <input type="text" placeholder={'brand'} {...register('brand', {*/}
-            {/*        // minLength: {value: 1, message: 'min 1 character'},*/}
-            {/*        // maxLength: {value: 20, message: 'max 20 characters'}*/}
-            {/*        pattern: {value: /^[a-zA-Zа-яА-яёЁіІїЇ]{1,20}$/, message: 'Only letters min 1 max 20 ch'}*/}
-            {/*    })}/>*/}
-            {/*    <input type="text" placeholder={'price'} {...register('price', {*/}
-            {/*        min: {value: 0, message: 'min 0'},*/}
-            {/*        max: {value: 1_000_000, message: 'max 1 000 000'},*/}
-            {/*        valueAsNumber: true*/}
-            {/*    })}/>*/}
-            {/*    <input type="text" placeholder={'year'} {...register('year', {*/}
-            {/*        min: {value: 1990, message: 'min 1990'},*/}
-            {/*        max: {value: new Date().getFullYear(), message: 'max current year'},*/}
-            {/*        valueAsNumber: true*/}
-            {/*    })}/>*/}
-            {/*    <button disabled={!isValid}>save</button>*/}
-            {/*</form>*/}
             <form onSubmit={handleSubmit(save)}>
                 <input type="text" placeholder={'brand'} {...register('brand')}/>
                 <input type="text" placeholder={'price'} {...register('price', {valueAsNumber: true})}/>
--- src/services/carService.js
+++ src/services/carService.js
@@ -3,7 +3,7 @@ import {urls} from "../constants/urls";
 
 const carService = {
     getAll: () => axiosService.get(urls.cars.base),
-    crate: (data) => axiosService.post(urls.cars.base, data),
+    create: (data) => axiosService.post(urls.cars.base, data),
     updateById: (id, data) => axiosService.put(urls.cars.byId(id), data),
     deleteById: (id) => axiosService.delete(urls.cars.byId(id))
 }

48e6dc920d1668bb61921e91a4b5a0f905940447
Mrkklnk <mrkk1nk1@gmail.com>
2023-11-16 20:41:33 +0100

    lesson7-test

---
+++ src/components/Characters.js
@@ -0,0 +1,32 @@
+import {useEffect} from "react";
+import {characterService} from "../services";
+import {useSearchParams} from "react-router-dom";
+import {useDispatch, useSelector} from "react-redux";
+
+import {characterActions} from "../redux/slices/characterSlice";
+import {Character} from "./Character";
+
+const Characters = () => {
+
+    const [query, setQuery] = useSearchParams({page: '1'})
+    const dispatch = useDispatch()
+    const {characters} = useSelector(state => state.characters)
+
+    const page = query.get('page')
+
+    useEffect(()=>{
+        setQuery({page:'1'})
+    },[setQuery])
+
+    useEffect(() => {
+        characterService.getAll(page).then(({data}) => dispatch(characterActions.setResponse(data)))
+    }, [page, dispatch])
+    return (
+        <div>
+            {characters.map(character => <Character key={character.id} character={character}/>)}
+
+        </div>
+    );
+};
+
+export {Characters};

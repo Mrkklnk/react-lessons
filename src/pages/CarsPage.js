2e346d5edb454c7d546e40811761a0d190198d87
Mrkklnk <mrkk1nk1@gmail.com>
2023-11-23 17:56:57 +0100

    lesson8

---
+++ src/pages/CarsPage.js
@@ -0,0 +1,16 @@
+import {CarForm, Cars} from "../components";
+import {useSelector} from "react-redux";
+
+const CarsPage = () => {
+    const {errors} = useSelector(state => state.cars)
+    return (
+        <div>
+            {errors&&<h1>{JSON.stringify(errors)}</h1>}
+            <CarForm/>
+            <hr/>
+            <Cars/>
+        </div>
+    );
+};
+
+export {CarsPage};
